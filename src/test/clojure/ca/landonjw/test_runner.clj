2c23246c86a59b1723f84ea6aeb24e1b173926e8
landonjw <landonjwdev@gmail.com>
2022-08-07 03:34:59 -0400

    Separate out integration tests, which should be run on live game server.

--- src/test/clojure/ca/landonjw/test_runner.clj
+++ src/test/clojure/ca/landonjw/test_runner.clj
@@ -1,6 +1,17 @@
 (ns ca.landonjw.test-runner
   (:require [clojure.test :refer :all]))
 
-(defn run []
-  (run-tests
-    'ca.landonjw.api.nbt-test))
+(def unit-tests
+  ['ca.landonjw.api.nbt-test])
+
+(def integration-tests
+  [])
+
+(defn run [namespaces]
+  (apply run-tests namespaces))
+
+(defn start [opts]
+  (condp = (:test-type opts)
+    :unit (run unit-tests)
+    :integration (run integration-tests)
+    :all (run (into unit-tests integration-tests))))
